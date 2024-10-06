3fa54b6b58d82f5b972586c31a1ec42f2768f7c7
photon256 <144157217+photon256@users.noreply.github.com>
2024-10-06 19:31:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def upload(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
-
+file_name = os.path.splitext
     path = f"./downloads/{m.chat.id}"
 
     try:
